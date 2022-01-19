40fe8b3de93b3a9ea03288abbcd6d468b6ae8301
Nicholas Payne <94993634+NFSnicholas@users.noreply.github.com>
2022-01-19 18:07:47 -0500

    first commit

--- JS/site.js
+++ JS/site.js
@@ -1,14 +1,52 @@
 
-// Dispalying Darren to unsuspecting Victims
-function getMessage(){
+//Collecting the Users Input------------------------------------------------------------------------------------------------
+function getText(){
 
-    let userMessage = document.getElementById("message").value;
-    
-Swal.fire(
-    'The App Title',
-    userMessage,
-    'question'
-    );
+    let userString = document.getElementById("userInput").value;
+    let isPalindrome = checkPalindrome(userString);
 
-    console.log(userMessage);
+
+    //Cekcing for Palindrome
+    checkPalindrome(userString);
+
+     //calling display data
+     displayData(isPalindrome);
+
 }
+
+//Check Palindrome function----------------------------------------------------------------------------------------------------------
+function checkPalindrome(userString){
+
+    userString = userString.toLowerCase();
+
+    let regex = /[^a-z0-9]/gi;
+
+    userString = userString.replace(regex, "");
+
+    let reverseString = ""
+    for (let index = userString.length -1; index >= 0; index--) { 
+        reverseString += userString[index];
+    }
+
+    if (reverseString == userString) {
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+
+//Display Data Function---------------------------------------------------------------------------------------------------------------
+function displayData(isPalindrome) {
+    if(isPalindrome == true){
+         Swal.fire('Neat! Your Phrase is a Palindrome');
+    }
+    else{ 
+        Swal.fire('OOPS! Your Phrase is not a Palindrome');
+    }
+
+}
+
+
+
